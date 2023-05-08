d4939b57b24b6df1a4e61bf4e226552f940d4b4b
Zeroual tibari <tibarinewdzign@gmail.com>
2023-05-08 23:01:55 +0200

    Implementing CircuitBreaker in Api Gatewaye with configuration

--- CloudGateway/src/main/java/com/arcesi/coudgateway/CloudGatewayApplication.java
+++ CloudGateway/src/main/java/com/arcesi/coudgateway/CloudGatewayApplication.java
@@ -2,12 +2,31 @@ package com.arcesi.coudgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
+import org.springframework.context.annotation.Bean;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 
 @SpringBootApplication
 public class CloudGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudGatewayApplication.class, args);
+		
 	}
+
+	@Bean
+    public Customizer<Resilience4JCircuitBreakerFactory> globalCustomConfiguration() {
+        
+        // the circuitBreakerConfig and timeLimiterConfig objects
+
+        return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
+          .timeLimiterConfig(TimeLimiterConfig.ofDefaults())
+          .circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
+          .build());
+    } 
 
 }
---
+++ CloudGateway/src/main/java/com/arcesi/coudgateway/controller/FallBackController.java
@@ -0,0 +1,23 @@
+package com.arcesi.coudgateway.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FallBackController {
+
+	@GetMapping(value="/orderServiceFallBack")
+	public String orderServiceFallBack() {
+		return "Order service is down!!";
+	}
+	
+	@GetMapping(value="/paymentServiceFallBack")
+	public String paymentServiceFallBack() {
+		return "Payment service is down!";
+	}
+	
+	@GetMapping(value="/productServiceFallBack")
+	public String productServiceFallBack() {
+		return "Product Service is down!";
+	}
+}
